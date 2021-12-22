d0a54e6b525a7d5ca7e91706e70541e8793d5b03
joaopedro-hub <joaop1580@gmail.com>
2021-12-22 13:17:04 -0300

    Desenvolvimento da home, controller e model

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,7 +31,7 @@
                             <a href="/" class="nav-link">Eventos</a>
                         </li>
                         <li class="nav-item">
-                            <a href="/" class="nav-link">Criar Eventos</a>
+                            <a href="/events/create" class="nav-link">Criar Eventos</a>
                         </li>
                         <li class="nav-item">
                             <a href="/" class="nav-link">Entrar</a>
@@ -46,6 +46,9 @@
         {{-- Aqui vai ficar conteúdo da página dinâmicamente --}}
         @yield('content')
 
+        
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
         <footer>
             <p>Eventos &copy; 2021</p>
         </footer>
