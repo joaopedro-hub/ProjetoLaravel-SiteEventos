59a910ea683a4ca02bb7ed5f38a255a1bed4569f
joaopedro-hub <joaop1580@gmail.com>
2021-12-23 19:16:33 -0300

    Desenvolvimento métodos store , show.Model,migration e controller.

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    //Itens vai ser tratado como array, não uma string
+    protected $casts = [
+        'itens' => 'array'
+    ];
+
+    //Para o laravel entender que o campo é do tipo date
+    protected $dates = ['date'];
 }
